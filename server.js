8a11db90b0c145e689c3003bf21d2b02919bd1cf
suma-gitrep <s537239@nwmissouri.edu>
2019-10-18 15:48:22 -0500

    made changes to port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')  // require built-in Node.js http package
 
 // Use hosting values if available, otherwise default 
 const hostname = process.env.hostname || '0.0.0.0' // allow remote access
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3003
 
 // define our server
 const server = http.createServer((req, res) => {
